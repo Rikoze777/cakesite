f0208a9b76b77305e0b69162d5abb19cf7aad22e
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-11-23 20:15:01 +0300

    Fix models, admin, makemigrations

--- cake/admin.py
+++ cake/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from .models import User
+from .models import (
+    User,
+    Cake,
+    Order,
+    Complaint,
+    Promocode,
+)
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("name", "phonenumber")
+
+
+@admin.register(Cake)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("words", "comments")
+
+
+@admin.register(Order)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("address", "total")
+
+
+@admin.register(Complaint)
+class UserComplaint(admin.ModelAdmin):
+    list_display = ("order", "message")
+
+
+@admin.register(Promocode)
+class Promocode(admin.ModelAdmin):
+    list_display = ("value", "discount")
--- cake/models.py
+++ cake/models.py
@@ -7,18 +7,69 @@ class User(models.Model):
     email = models.EmailField('Email', max_length=70, blank=True, unique=True)
     phonenumber = PhoneNumberField('Номер телефона', blank=True)
 
+    def __str__(self) -> str:
+        return f"@{self.name}, {self.phonenumber}"
+
 
 class Cake(models.Model):
-    levels = models.IntegerField('Количество уровней торта', default=1)
-    form = models.CharField('Форма торта', max_length=50)
-    topping = models.CharField(
-        'Топпинг',
-        max_length=50,
-        default='Без топпинга')
-    berries = models.CharField('Ягоды', max_length=50, default='Без ягод')
-    decor = models.CharField('Декор', max_length=50, default='Без декора')
+    class Form(models.TextChoices):
+        ROUND = "ROUND"
+        SQUARE = "SQUARE"
+        RECTANGLE = "RECTANGLE"
+        NO = "NO"
+
+    class Levels(models.IntegerChoices):
+        ZERO = 0
+        ONE = 1
+        TWO = 2
+        THREE = 3
+
+    class Toppings(models.TextChoices):
+        WITHOUT = "WITHOUT"
+        WHITE = "WHITE"
+        CARAMEL = "CARAMEL"
+        MAPLE = "MAPLE"
+        BLUEBERRY = "BLUEBERRY"
+        MILKCHOCKOLATE = "MILKCHOCKOLATE"
+        STAWBERRY = "STAWBERRY"
+        NO = "NO"
+
+    class Berries (models.TextChoices):
+        STAWBERRY = "STAWBERRY"
+        BLUEBERRY = "BLUEBERRY"
+        RAPSBERRY = "RAPSBERRY"
+        BLACKBERRY = "BLACKBERRY"
+        NO = "NO"
+
+    class Decors (models.TextChoices):
+        PISTACHIO = "PISTACHIO"
+        MERINGUE = "MERINGUE"
+        HAZELNUT = "HAZELNUT"
+        PECAN = "PECAN"
+        MARSHMALLOW = "MARSHMALLOW"
+        MARZIPAN = "MARZIPAN"
+        NO = "NO"
+
+    levels = models.IntegerField('Количество уровней торта',
+                                 choices=Levels.choices,
+                                 default=Levels.ZERO)
+    form = models.CharField('Форма торта', max_length=50,
+                            choices=Form.choices,
+                            default=Form.NO)
+    toppings = models.CharField('Топпинг', max_length=50,
+                                choices=Toppings.choices,
+                                default=Toppings.NO)
+    berries = models.CharField('Ягоды', max_length=50,
+                               choices=Berries.choices,
+                               default=Berries.NO)
+    decors = models.CharField('Декор', max_length=50,
+                              choices=Decors.choices,
+                              default=Decors.NO)
     words = models.CharField('Надпись', max_length=50, blank=True)
     comments = models.TextField('Комментарий к заказу', blank=True)
+
+    def __str__(self) -> str:
+        return f"@{self.words}, {self.comments}"
 
 
 class Order(models.Model):
@@ -39,6 +90,9 @@ class Order(models.Model):
     delivery_datetime = models.DateTimeField('Дата и время доставки')
     total = models.IntegerField('Стоимость заказа')
 
+    def __str__(self) -> str:
+        return f"@{self.address}, {self.total}"
+
 
 class Complaint(models.Model):
     message = models.TextField('Текст жалобы', blank=True)
@@ -49,9 +103,15 @@ class Complaint(models.Model):
         on_delete=models.CASCADE,
         null=True)
 
+    def __str__(self) -> str:
+        return f"@{self.order}, {self.message}"
+
 
 class Promocode(models.Model):
     start_date = models.DateField('Дата начала действия промокода')
     end_date = models.DateField('Дата окончания действия промокода')
     discount = models.FloatField('Процент скидки')
     value = models.CharField('Текст промокода', max_length=50)
+
+    def __str__(self) -> str:
+        return f"@{self.value}, {self.discount}"
