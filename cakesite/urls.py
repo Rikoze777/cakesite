0b1d6b82d71b837219af058b278bd5a1b1f85e57
Den Kasyanov <den@den.ai>
2022-11-24 17:34:19 +0300

    Add index view

--- cakesite/urls.py
+++ cakesite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from cake.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
     path('cake/', include('cake.urls')),
+
 ]
