4820b6908fed2a5ad4c25fb60415252f6cccef55
Den Kasyanov <den@den.ai>
2022-11-24 18:54:40 +0300

    Add static urls for debug

--- cakesite/urls.py
+++ cakesite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from cake.views import index
 
@@ -22,5 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('cake/', include('cake.urls')),
-
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
