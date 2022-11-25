ca9a5cd022d31e5d5901ad6bc792b2686bfa80ba
Rikoze777 <61386361+Rikoze777@users.noreply.github.com>
2022-11-25 13:57:51 +0300

    auto deploy

--- cakesite/settings.py
+++ cakesite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "REPLACE_ME")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "true").lower() in ['yes', '1', 'true']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ovz1.j72597015.n03kn.vps.myjino.ru"]
 
 
 # Application definition
